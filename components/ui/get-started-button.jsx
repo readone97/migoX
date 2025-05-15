418ccb108a736e94e62075f947854abf61d0d575
Ridwan O. Abdullateef <49338477+readone97@users.noreply.github.com>
2025-05-15 21:08:41 +0100

    Update get-started-button.jsx

--- components/ui/get-started-button.jsx
+++ components/ui/get-started-button.jsx
@@ -12,21 +12,21 @@ const GetStartedButton = () => {
       
       {/* Button with Link */}
       <div className="relative">
-        {/* <Link href="/login" className="block w-full">
+         <Link href="/login" className="block w-full">
           <Button  
            className="relative overflow-hidden group bg-[#23C69A] hover:bg-[#23C69A]/90 text-[#1B1B1B]">
             <span>Get Started</span>
             <ArrowRightIcon className="w-4 h-4" />
           </Button>
-        </Link> */}
-         <Button            
+        </Link> 
+{/*          <Button            
             className="relative overflow-hidden group bg-[#23C69A] hover:bg-[#23C69A]/90 text-[#1B1B1B]"         >
              <Link href="/login">
                <span className="relative z-10">Get Started</span>
              <span className="absolute inset-0 bg-white/10 transform translate-y-full group-hover:translate-y-0 transition-transform duration-300"></span>
                <ArrowRight className="ml-2 h-4 w-4 transition-transform duration-300 group-hover:translate-x-1" />
              </Link>
-            </Button>
+            </Button> */}
       </div>
     </div>
   );
